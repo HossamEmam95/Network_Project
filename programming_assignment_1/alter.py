31bcf4fc39350cebce4bfc5c287b42fa8ca2a5f8
Ahmed Bahgat <33203944+A7medBahgat@users.noreply.github.com>
2018-11-21 23:42:16 +0200

    Add files via upload

---
+++ programming_assignment_1/alter.py
@@ -0,0 +1,17 @@
+def alter(generated_message: str, index: int) -> str:
+    """
+       Return the input genrated_message after inverting the indexed character
+       args:
+           generated_message(string): m-bit message consisting of a string
+           of 0s and 1s.
+           index(int): the index of the character in the generated message
+           to be inverted
+    """
+    if(index >= len(generated_message) or index < 0):
+        return generated_message
+    generated_message = list(generated_message)
+    if(generated_message[index] == '1'):
+        generated_message[index] = '0'
+    else:
+        generated_message[index] = '1'
+    return ''.join(generated_message)
